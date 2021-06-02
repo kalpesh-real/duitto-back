d1e63cea8196baf2e5bca18bfeae94ef2399183e
kalpesh-real <61679981+kalpesh-real@users.noreply.github.com>
2021-06-02 21:48:02 +0530

    Update Test.java

--- src/main/java/com/Duitto/model/Test.java
+++ src/main/java/com/Duitto/model/Test.java
@@ -23,7 +23,7 @@ public class Test {
 		@Column(name = "customer_id" ,columnDefinition = "long default 0")
 		private long customerId;
 
-		@Column(name = "username")
+		@Column(name = "username",columnDefinition = "text")
 		private String reservedUName;
 		
 		@Column(name = "available" ,columnDefinition = "integer default 0")
